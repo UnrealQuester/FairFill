816fc0a6fd94dc5d71fda0ba4583fcdafdcd9bdf
UnrealQuester <UnrealQuester@users.noreply.github.com>
2014-11-06 10:41:50 +0100

    Update main.cpp
    Added user input

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 #include <map>
 #include <vector>
 #include <numeric>
+#include <sstream> 
 
 using namespace std;
 
-class Becher {
+class Bucket {
 
 public:
 
-    Becher(int max, int actual) : max(max), actual(actual){}
+    Bucket(int max, int actual) : max(max), actual(actual){}
     bool isEmpty() {
         return actual == 0;
     }
@@ -23,15 +24,15 @@ public:
     int actual;
 };
 
-const Becher NullBecher(0, 0);
+const Bucket NullBucket(0, 0);
 
-typedef std::vector<Becher> WeinBecher;
+typedef std::vector<Bucket> WineBucket;
 
-bool operator==(const Becher &a, const Becher &b) {
+bool operator==(const Bucket &a, const Bucket &b) {
     return a.max == b.max && a.actual == b.actual;
 }
 
-const bool operator<(const Becher &a, const Becher &b) {
+const bool operator<(const Bucket &a, const Bucket &b) {
     if(a.max == b.max) {
         return a.actual < b.actual;
     } else {
@@ -39,11 +40,34 @@ const bool operator<(const Becher &a, const Becher &b) {
     }
 }
 
-Becher operator+(const Becher &a, const Becher &b) {
-    return Becher(a.max + b.max, a.actual + b.actual);
+Bucket operator+(const Bucket &a, const Bucket &b) {
+    return Bucket(a.max + b.max, a.actual + b.actual);
 }
 
-void exchange(Becher &a, Becher &b) {
+struct Exchange {
+    int von;
+    int zu;
+};
+
+
+class Steps {
+    public:
+    Steps(std::vector<Exchange> steps, bool valid) : steps(steps), valid(valid){}
+    std::vector<Exchange> steps;
+    bool valid;
+};
+
+bool operator<(const Steps &a, const Steps &b) {
+    if(!b.valid)
+        return true;
+    if(!a.valid)
+        return false;
+    return a.steps.size() < b.steps.size();
+}
+
+Steps null = Steps(std::vector<Exchange>(), false);
+
+void exchange(Bucket &a, Bucket &b) {
     int diff = b.max - b.actual;
     if(diff > a.actual) {
         b.actual += a.actual;
@@ -54,113 +78,125 @@ void exchange(Becher &a, Becher &b) {
     }
 }
 
-bool isBalanced(const WeinBecher &becher, int first) {
+bool isBalanced(const WineBucket &bucket, int first) {
     return (
-        std::accumulate(becher.begin(), becher.begin()+first, NullBecher).actual
-        == std::accumulate(becher.begin()+first, becher.end(), NullBecher).actual);
+        std::accumulate(bucket.begin(), bucket.begin()+first, NullBucket).actual
+        == std::accumulate(bucket.begin()+first, bucket.end(), NullBucket).actual);
 }
 
-void printBecher(const WeinBecher &becher) {
-    for(auto a = becher.begin(); a != becher.end(); a++) {
+void printBucket(const WineBucket &bucket) {
+    for(auto a = bucket.begin(); a != bucket.end(); a++) {
         cout << (*a).actual << "(" << (*a).max << ") ";
     }
+	cout << endl;
 }
 
-struct Tausch {
-    int von;
-    int zu;
-};
-
-
-class Abfolge {
-    public:
-    Abfolge(std::vector<Tausch> abfolge, bool valid) : abfolge(abfolge), valid(valid){}
-    std::vector<Tausch> abfolge;
-    bool valid;
-};
-
-bool operator<(const Abfolge &a, const Abfolge &b) {
-    if(!b.valid)
-        return true;
-    if(!a.valid)
-        return false;
-    return a.abfolge.size() < b.abfolge.size();
-}
-
-Abfolge null = Abfolge(std::vector<Tausch>(), false);
-
-void printExchanges(Abfolge abfolge) {
-    for(int i = 0; i < abfolge.abfolge.size(); i++) {
-        cout << abfolge.abfolge[i].von << " " << abfolge.abfolge[i].zu << endl;
+void printExchanges(Steps steps, WineBucket bucket) {
+    for(int i = steps.steps.size()-1; i >= 0; i--) {
+		printBucket(bucket);
+        cout << "Put the contents of bucket number "
+			 << (steps.steps[i].von+1)
+			 << " into bucket number "
+			 << (steps.steps[i].zu+1)
+			 <<  endl;
+		exchange(bucket[steps.steps[i].von], bucket[steps.steps[i].zu]);
     }
+	printBucket(bucket);
 }
 
-Abfolge umfullen(WeinBecher becher, int first, std::map<WeinBecher, int> &map, int depth) {
-    auto iter = map.find(becher);
+Steps solve(WineBucket bucket, int first, std::map<WineBucket, int> &map, int depth) {
+    auto iter = map.find(bucket);
     if(iter != map.end()) {
         if((*iter).second <= depth)
         return null;
     }
-    map[becher] = depth;
-    if(isBalanced(becher, first))
-        return Abfolge(std::vector<Tausch>(), true);
-    std::vector<Tausch> tausche;
-    for(int i = 0; i < becher.size(); i++) {
-        for(int j = 0; j < becher.size(); j++) {
+    map[bucket] = depth;
+    if(isBalanced(bucket, first))
+        return Steps(std::vector<Exchange>(), true);
+    std::vector<Exchange> exchanges;
+    for(int i = 0; i < bucket.size(); i++) {
+        for(int j = 0; j < bucket.size(); j++) {
             if(i == j) {
                 continue;
             }
-            if(becher[i].isEmpty() || becher[j].isFull()) {
+            if(bucket[i].isEmpty() || bucket[j].isFull()) {
                 continue;
             }
-            Tausch tausch = {i, j};
-            tausche.push_back(tausch);
+            Exchange ex = {i, j};
+            exchanges.push_back(ex);
         }
     }
-    Abfolge shortest = null;
-    for(int i = 0; i < tausche.size(); i++) {
-        WeinBecher b = becher;
-        exchange(b[tausche[i].von], b[tausche[i].zu]);
-        Abfolge solution = umfullen(b, first, map, depth+1);
+    Steps shortest = null;
+    for(int i = 0; i < exchanges.size(); i++) {
+        WineBucket b = bucket;
+        exchange(b[exchanges[i].von], b[exchanges[i].zu]);
+        Steps solution = solve(b, first, map, depth+1);
         if(solution.valid)
-            solution.abfolge.push_back(tausche[i]);
+            solution.steps.push_back(exchanges[i]);
         if(solution < shortest)
             shortest = solution;
     }
     return shortest;
 }
 
-void test(WeinBecher becher, int first, bool expected) {
-    std::map<WeinBecher, int> map;
-    auto tausche = umfullen(becher, first, map, 0);
-    printExchanges(tausche);
+void test(WineBucket bucket, int first) {
+	cout << "Please wait ..." << endl;
+    std::map<WineBucket, int> map;
+    auto exchange = solve(bucket, first, map, 0);
+	if(!exchange.valid) {
+		cout << "No solution!" << endl;
+		return;
+	}
+	if(exchange.steps.size() == 0) {
+		cout << "Already evenly distributed!" << endl;
+		return;
+	}
+	cout << exchange.steps.size() << " Schritte" << endl;
+    printExchanges(exchange, bucket);
     cout << endl << endl;
-    exit(0);
 }
 
 int main() {
-    {
-        WeinBecher becher;
-        becher.push_back(Becher(8, 8));
-        becher.push_back(Becher(5, 0));
-        becher.push_back(Becher(3, 0));
-        test(becher, 1, true);
-    }
-    {
-        WeinBecher becher;
-        becher.push_back(Becher(10, 10));
-        becher.push_back(Becher(8, 4));
-        becher.push_back(Becher(11, 4));
-        becher.push_back(Becher(7, 6));
-        test(becher, 1, false);
-    }
-    {
-        WeinBecher becher;
-        becher.push_back(Becher(6, 0));
-        becher.push_back(Becher(26, 0));
-        becher.push_back(Becher(13, 0));
-        becher.push_back(Becher(50, 20));
-        test(becher, 2, true);
-    }
-    return 0;
+	int first;
+	int n;
+	vector<int> capacity;
+	vector<int> content;
+	WineBucket bucket;
+	string line;
+	cout << "Please enter the number of buckets the first guy has" << endl;
+	cin >> first;
+	cout << "Please enter the maximum capacity of the buckets" << endl;
+	getline(cin,line);
+	getline(cin,line);
+	stringstream ss = stringstream(line);
+	while(ss >> n) {
+		if(n < 1) {
+			cout << "The capacity must be at least 1" << endl;
+			exit(1);
+		}
+		capacity.push_back(n);
+	}
+	cout << "Please enter the content of the buckets" << endl;
+	getline(cin,line);
+	ss = stringstream(line);
+	while(ss >> n) {
+		if(n < 0) {
+			cout << "You may only enter natural numbers" << endl;
+			exit(1);
+		}
+		content.push_back(n);
+	}
+	for(int i = 0; i < content.size() && i < capacity.size(); i++) {
+		if(capacity[i] < content[i]) {
+			cout << "A Bucket must be able to hold its contents" << endl;
+			exit(1);
+		}
+		bucket.push_back(Bucket(capacity[i], content[i]));
+	}
+	if(first < 1 || first >= bucket.size()) {
+		cout << "The first guy has to have between 1 and " << bucket.size() -1 << " buckets" << endl;
+		exit(1);
+	}
+	test(bucket, first);
+	return 0;
 }
